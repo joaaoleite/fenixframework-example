2354ad14c3427c7789c1ce3a0c141447a3160bd1
joaaoleite <joaogsleite@gmail.com>
2016-02-25 15:50:04 +0000

    fix folder structure for MyDrive (missing pom.xml)

---
+++ src/main/java/pt/tecnico/mydrive/MyDriveApplication.java
@@ -0,0 +1,71 @@
+package pt.tecnico.mydrive;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.File;
+
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.FenixFramework;
+
+public class MyDriveApplication {
+    static final Logger log = LogManager.getRootLogger();
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("*** Welcome to the MyDrive application! ***");
+	      try {
+	          setup();
+	          for (String s: args) xmlScan(new File(s));
+	              print();
+	          xmlPrint();
+	      } finally { FenixFramework.shutdown(); }
+    }
+
+    @Atomic
+    public static void init() { // empty mydrive
+        log.trace("Init: " + FenixFramework.getDomainRoot());
+	      MyDrive.getInstance().cleanup();
+    }
+
+    @Atomic
+    public static void setup() { // mydrive with debug data
+        log.trace("Setup: " + FenixFramework.getDomainRoot());
+	      MyDrive mydrive = MyDrive.getInstance();
+    }
+
+    @Atomic
+    public static void print() {
+        log.trace("Print: " + FenixFramework.getDomainRoot());
+	      MyDrive mydrive = MyDrive.getInstance();
+	  }
+
+    @Atomic
+    public static void xmlPrint() {
+        log.trace("xmlPrint: " + FenixFramework.getDomainRoot());
+	      Document doc = MyDrive.getInstance().xmlExport();
+	      XMLOutputter xmlOutput = new XMLOutputter(Format.getPrettyFormat());
+	      try { xmlOutput.output(doc, new PrintStream(System.out)); } 
+        catch (IOException e) { System.out.println(e); }
+    }
+
+    @Atomic
+    public static void xmlScan(File file) {
+        log.trace("xmlScan: " + FenixFramework.getDomainRoot());
+	      MyDrive mydrive = MyDrive.getInstance();
+	      SAXBuilder builder = new SAXBuilder();
+	      try {
+	          Document document = (Document)builder.build(file);
+	          mydrive.xmlImport(document.getRootElement());
+	      } catch (JDOMException | IOException e) {
+	          e.printStackTrace();
+	      }
+    }
+}
