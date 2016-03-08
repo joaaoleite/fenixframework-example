5c738bb5e75918b0f39922f3731f46101d96a6da
joaotomazio <jdtomazio@gmail.com>
2016-03-08 20:28:58 +0000

    File constructor and File Logic Done

--- src/main/java/pt/tecnico/mydrive/domain/File.java
+++ src/main/java/pt/tecnico/mydrive/domain/File.java
@@ -2,8 +2,23 @@ package pt.tecnico.mydrive.domain;
 
 public class File extends File_Base {
     
-    public File() {
+    public File(MyDrive mydrive, Dir parent, User owner, String name, DateTime last_mod, String mask) {
         super();
+
+        setMydrive(mydrive);
+        setParent(parent);
+        setOwner(owner);
+
+        int id = mydrive.getNfile();
+        setId(id);
+        
+        //Implement name restrictions
+        setName(name);
+        setLast_modification(last_mod);
+        setMask(mask);
     }
-    
-}
+
+    protected abstract int getSize();
+    protected abstract void remove(String name);
+
+
