c3b3dbbd98a8c59e40c1e4ef857a54c2bdef7cc1
TonyFreire <tonifreire93@gmail.com>
2016-03-09 21:08:12 +0000

    User constructor done

--- src/main/java/pt/tecnico/mydrive/domain/User.java
+++ src/main/java/pt/tecnico/mydrive/domain/User.java
@@ -1,9 +1,17 @@
 package pt.tecnico.mydrive.domain;
 
 public class User extends User_Base {
-    
-    public User() {
-        super();
-    }
+    private Dir home;
+    public User(String username, String name, String password,String umask ) {
+        init(username, name, password, umask);
+        //home = new Dir();
+     }
+    protected void init(String username, String name, String password,String umask ) {
+        setUsername(username);
+        setName(name);
+        setPassword(password);
+        setUmask(umask);
+   }
+
     
 }
