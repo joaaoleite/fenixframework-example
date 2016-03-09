e7473541f53e5f980e070a2ec3bed481b15ec34a
tiagofbfernandes <tiagofbfernandes@gmail.com>
2016-03-09 15:00:53 +0000

    Started Dir

--- src/main/java/pt/tecnico/mydrive/domain/Dir.java
+++ src/main/java/pt/tecnico/mydrive/domain/Dir.java
@@ -5,4 +5,17 @@ public class Dir extends Dir_Base {
     public Dir() {
         super();
     }
+
+    protected getFile() throws FileDoesNotExistException{
+
+    }
+
+    
+    @override
+    protected void remove() throws DirectoryIsNotEmptyException{
+    	if(getFileSet().isEmpty()){
+    		throw new DirectoryIsNotEmptyException();
+    	}
+    	super.remove();
+    }  
 }
