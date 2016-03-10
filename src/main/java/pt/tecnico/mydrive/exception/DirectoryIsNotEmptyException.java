0f4b5fb6e1c31f22d79ecaa7975a34bcb43d833e
laurajbarroso <laurajbarroso@gmail.com>
2016-03-10 21:38:32 +0000

    create DirectoryIsNotEmptyException

---
+++ src/main/java/pt/tecnico/mydrive/exception/DirectoryIsNotEmptyException.java
@@ -0,0 +1,25 @@
+package pt.tecnico.mydrive.exception;
+
+
+public class DirectoryIsNotEmptyException extends MyDriveException {
+
+    
+    private static final long serialVersionUID = 1L;
+
+
+	private final String _name;
+
+	/**
+	 * @param name
+	 */
+	public DirectoryIsNotEmptyException(String name) {
+		_name = name;
+	}
+
+	
+	@Override
+	@SuppressWarnings("nls")
+	public String getMessage() {
+		return "The Directory:" + _name + " is not empty.";
+	}
+}
