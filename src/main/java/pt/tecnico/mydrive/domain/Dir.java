ebeaa6c0c38f5fd668bb156e7638589f2836496c
Luis Pereira <luispereira1795@hotmail.com>
2016-03-10 22:17:04 +0000

    Added Export and Import XML

--- src/main/java/pt/tecnico/mydrive/domain/Dir.java
+++ src/main/java/pt/tecnico/mydrive/domain/Dir.java
@@ -1,32 +1,46 @@
+
 package pt.tecnico.mydrive.domain;
+
+import pt.tecnico.mydrive.exception.*;
 
 public class Dir extends Dir_Base {
     
     protected Dir(MyDrive mydrive, Dir parent, User owner, String name, String mask) {
-        super(mydrive, parent, owner, name, mask);
+        init(mydrive, parent, owner, name, mask);
     }
 
     protected File getFileByName(String name) throws FileDoesNotExistException{
-        for (File file: getFileSet())
+        for (File file: getFileSet()){
+            if (file.getName().equals("."))
+                return this;
+            if (file.getName().equals(".."))
+                return getParent();
+
             if (file.getName().equals(name))
                 return file;
-        throw new FileDoesNotExistException();
+        }
+
+        throw new FileDoesNotExistException(name);
     } 
 
     protected Dir getDir(String name) throws FileDoesNotExistException, FileIsAPlainFileException{
         if (!exists(name))
-            throw FileDoesNotExistException;
+            throw new FileDoesNotExistException(name);
         
         File file = getFileByName(name);
         if (file.isDir())
-            return file;
+            return (Dir) file;
 
-        return FileIsAPlainFileException();
+        throw new FileIsAPlainFileException(name);
+    }
+
+    protected boolean isDir(){
+        return true;
     }
 
     protected Boolean exists(String name){
         try{
-            File file = getFileByName(name);
+            getFileByName(name);
             return true;
         }
         catch(FileDoesNotExistException e){
@@ -40,74 +54,107 @@ public class Dir extends Dir_Base {
             addFile(newDir);
             return newDir;  
         }
-        throw new FileAlreadyExistsException();
+        throw new FileAlreadyExistsException(name);
     }
 
     protected PlainFile createPlainFile(User owner, String name, String mask) throws FileAlreadyExistsException{
         if (!exists(name)){
             return new PlainFile(getMydrive(), this, owner, name, mask);
         }    
-        throw new FileAlreadyExistsException();
+        throw new FileAlreadyExistsException(name);
     }
 
-    protected PlainFile createPlainFile(User owner, String name, String mask, String content) throws FileAlreadyExistsException{
-        Plainfile newPlainFile = createPlainFile(getMydrive(), this, owner, name, mask);
-        newPlainFile.write(content);
-        return newPlainFile;    
+    protected PlainFile createPlainFile(User owner, String name, String content) throws FileAlreadyExistsException{
+        if (!exists(name)){
+            PlainFile plain = new PlainFile(getMydrive(), this, owner, name, mask);
+            plain.write(content);
+            return plain;
+        }    
+        throw new FileAlreadyExistsException(name);
     }
 
     private String listDir(){
         String output = (type()+" "+getMask()+" "+getSize()+" "+getOwner().getUsername()+" "+getId()+" "+getLastModification()+" "+getName()+"\n"
             +getParent().type()+" "+getParent().getMask()+" "+getParent().getSize()+" "+getParent().getOwner().getUsername()+" "+getParent().getId()+" "+getParent().getLastModification()+" "+getParent().getName()+"\n");
+        
         for (File file: getFileSet()){
-            output += (file.type()+" "+file.getMask()+" "+file.getSize()+" "+file.getOwner().getUsername()+" "+file.getId()+" "+file.getLastModification()+" "+file.getName()+"\n");
-            return output;
+            if (file instanceof Link){
+                Link link = (Link) file;
+                output += (link.getName()+"->"+link.getContent());
+            }
+            else
+                output += (file.type()+" "+file.getMask()+" "+file.getSize()+" "+file.getOwner().getUsername()+" "+file.getId()+" "+file.getLastModification()+" "+file.getName()+"\n");
         }
+        return output;
     }
 
-    private Int getDirSize(){
-        return (2 + getFileSet.size());
+    protected int getSize(){
+        return (2 + getFileSet().size());
     }
     
     @Override
     protected void remove() throws DirectoryIsNotEmptyException{
-    	if(getFileSet().isEmpty()){
-    		throw new DirectoryIsNotEmptyException();
-    	}
-    	super.remove();
+        if(getFileSet().isEmpty()){
+            throw new DirectoryIsNotEmptyException(getName());
+        }
+        super.remove();
     }  
 
-    private Dir xmlCreateDir(User owner, String name, String mask){
-        if(exists(name)){
-            File file = getFileByName(name);
-            if(file.isDir()){
-                return file;
-            }
-            else{
-                throw new  FileAlreadyExistsException();
-            }  
-        }
-        return createDir(owner,name,mask);
-    }
+    protected void removeR() throws DirectoryIsNotEmptyException{
+        for(File f: getFileSet()){
+            f.removeR();
+        }
+        remove();
+    }
 
-    public void xmlImport(Element dirElement) {
-        for (Element e: directory.getChildren("dir"){
-            String name = new String(fileElement.getAttribute("name").getValue().getBytes("UTF-8"));
-            User owner = getMydrive().getUserByUsername(new String(fileElement.getAttribute("owner").getValue().getBytes("UTF-8")));
-            String mask = new String(fileElement.getAttribute("mask").getValue().getBytes("UTF-8"));
-            Dir dir = createDir(owner, name, mask);
-            dir.xmlImport(e);
+    private Dir xmlCreateDir(User owner, String name, String mask) throws FileAlreadyExistsException{
+        if(exists(name)){
+            File file = getFileByName(name);
+            if(file.isDir()){
+                return (Dir) file;
+            }
+            else{
+                throw new FileAlreadyExistsException();
+            }
+        }
+        return createDir(owner,name,mask);
+    }
+
+    protected String type(){
+        return "Directory";
+    }
+
+    public void xmlImport(Element dirElement) throws ImportDocException{
+        try{
+            for (Element e: directory.getChildren("dir"){
+                String name = new String(fileElement.getAttribute("name").getValue().getBytes("UTF-8"));
+                User owner = getMydrive().getUserByUsername(new String(fileElement.getAttribute("owner").getValue().getBytes("UTF-8")));
+                String mask = new String(fileElement.getAttribute("mask").getValue().getBytes("UTF-8"));
+                Dir dir = createDir(owner, name, mask);
+                dir.xmlImport(e);
+            }
+        } catch (UnsupportedEncodingException e) { 
+            System.err.println(e); 
+            throw new ImportDocException(); 
         }
     }
 
-    public Element xmlExport(){
-        Element element = new Element("dir");
-        element.setAttribute("name", getName());
-        element.setAttribute("owner",getOwner());
-        element.setAttribute("mask",getMask());
+    public Element xmlExport() throws ExportDocException{
+        try{
+            Element element = new Element("dir");
+            element.setAttribute("name", getName());
+            element.setAttribute("owner",getOwner());
+            element.setAttribute("mask",getMask());
 
-        for(File f: getFileSet())
-            element.addContent(f.xmlExport());
+            for(File f: getFileSet())
+                element.addContent(f.xmlExport());
+        } catch (UnsupportedEncodingException e) { 
+            System.err.println(e); 
+            throw new ExportDocException(); 
+        }  
         return element;
     }
+
+
+
 }
