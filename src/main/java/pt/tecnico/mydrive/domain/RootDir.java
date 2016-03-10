54ff343aa28275ae90f5022e2d4b36cee2cb1049
joaotomazio <jdtomazio@gmail.com>
2016-03-10 11:35:24 +0000

    rootdir setowner with overridin

--- src/main/java/pt/tecnico/mydrive/domain/RootDir.java
+++ src/main/java/pt/tecnico/mydrive/domain/RootDir.java
@@ -2,16 +2,24 @@ package pt.tecnico.mydrive.domain;
 
 public class RootDir extends RootDir_Base {
     
-    public RootDir(MyDrive mydrive, String mask){
-        super(mydrive, null, null, "/", mask);
+    public RootDir(MyDrive mydrive){
+        super(mydrive, null, null, "/", "");
         
-        createDir(null, "home", mask);
+        createDir(null, "home", "");
 
     }
     
     @Override
-    protected Dir getParent(){
+    private Dir getParent(){
         return this;            
     }
-
+    
+    @Overrride
+    private void setOwner(SuperUser user){
+        super.setOwner(user);
+        Dir home = getDir("home");
+        removeFile(home);
+        home.setOwner(SuperUser user);
+        addFile(home);
+    
 }
