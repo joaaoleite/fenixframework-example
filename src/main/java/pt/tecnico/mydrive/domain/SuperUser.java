3b64f2fe5778463e7a42e78ddf925fac04374b2d
TonyFreire <tonifreire93@gmail.com>
2016-03-10 14:43:38 +0000

    new user constructor

--- src/main/java/pt/tecnico/mydrive/domain/SuperUser.java
+++ src/main/java/pt/tecnico/mydrive/domain/SuperUser.java
@@ -1,11 +1,8 @@
 package pt.tecnico.mydrive.domain;
 
 public class SuperUser extends SuperUser_Base {
-    public SuperUser(MyDrive myDrive) {
-        RootDir rootDir =new RootDir (this);
-        Dir home = rootDir.createDir("home",this,null)
-        myDrive.setRootDir(rootDir); 
-        init("root", "SuperUser", "***","rwxdr-x-",home ); 
+    public SuperUser(MyDrive myDrive) { 
+        init("root", "SuperUser", "***","rwxdr-x-",myDrive.getRootDir()); 
     }
     
 }
--- src/main/java/pt/tecnico/mydrive/domain/User.java
+++ src/main/java/pt/tecnico/mydrive/domain/User.java
@@ -1,26 +1,28 @@
 package pt.tecnico.mydrive.domain;
 
+
 public class User extends User_Base {
     public User(MyDrive myDrive ,String username ) {
-        //name=username    
-        init(username, username, username," rwxd----",myDrive.getRootDir);
+        init(username, username, username,"rwxd----",myDrive.getRootDir());
     }
     public User(MyDrive myDrive ,String username, String umask ) {
-        //name=username    
-        init(username, username, username, umask,myDrive.getRootDir);
+        init(username, username, username,umask,myDrive.getRootDir());
     }
     
-    public User(MyDrive myDrice, String username, String password,String umask ) {
-        init(username, username, password, umask,home, myDrive.getRootDir);
+    public User(MyDrive myDrive ,String username, String password) {
+        init(username, username, password,"rwxd----",myDrive.getRootDir());
+    }
+    public User(MyDrive myDrive, String username, String password,String umask ) {
+        init(username, username, password,umask,home, myDrive.getRootDir());
     }    
    
  
-    protected void init(String username, String name, String password,String umask, Dir home ) {
+    protected void init(MyDrive myDrive,String username, String name, String password,String umask, Dir home ) {
         setUsername(username);
         setName(name);
         setPassword(password);
         setUmask(umask);
         setHomedir(home);
-    }
+
 }     
 
