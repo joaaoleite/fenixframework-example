907524c8a7aca35a7c1c4a7497a60c63ca0d0166
laurajbarroso <laurajbarroso@gmail.com>
2016-03-10 21:09:56 +0000

    create FileIsAPlainFileException

---
+++ src/main/java/pt/tecnico/mydrive/exception/FileIsAPlainFileException.java
@@ -0,0 +1,25 @@
+package pt.tecnico.mydrive.exception;
+
+
+public class FileIsAPlainFileException extends MyDriveException {
+
+    
+    private static final long serialVersionUID = 1L;
+
+
+	private final String _name;
+
+	/**
+	 * @param name
+	 */
+	public FileIsAPlainFileException(String name) {
+		_name = name;
+	}
+
+	
+	@Override
+	@SuppressWarnings("nls")
+	public String getMessage() {
+		return "The File:" + _name + " is a PlainFile.";
+	}
+}
