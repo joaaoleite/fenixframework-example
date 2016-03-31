dc2f62d343a1f85a990157fd2d23ed952a77c44b
joaotomazio <jdtomazio@gmail.com>
2016-03-31 13:49:21 +0100

    createfileservice skeleton

---
+++ src/main/java/pt/tecnico/mydrive/service/CreateFileService.java
@@ -0,0 +1,16 @@
+package pt.tecnico.mydrive.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import pt.ist.fenixframework.Atomic;
+import pt.tecnico.mydrive.domain.*;
+import pt.tecnico.mydrive.exception.*;
+
+public class CreateFileService extends MyDriveService{
+
+    private void dispatch() throws MyDriveException{
+
+    }
+
+}
