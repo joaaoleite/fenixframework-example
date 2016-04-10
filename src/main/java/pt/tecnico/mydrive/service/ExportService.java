dfaa4cdfbc046fb54500866e64e37ba52d443465
Laura Barroso <laurajbarroso@gmail.com>
2016-04-10 15:51:57 +0100

    Starting ExportService

---
+++ src/main/java/pt/tecnico/mydrive/service/ExportService.java
@@ -0,0 +1,34 @@
+package pt.tecnico.mydrive.service;
+
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import pt.tecnico.mydrive.domain.*;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.Attribute;
+
+public class ExportService extends AbstractServiceTest {
+
+
+
+ public ExportService() {
+        
+        private File file;
+    	private Set<Users> users;
+    }
+    
+
+    public ExportService() {
+        users = new HashSet<Users>();
+    }
+
+    public void add(String filename) {
+        users.add(filename);
+    }
+
+
