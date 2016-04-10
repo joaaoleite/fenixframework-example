870dbb37e915b2670355c521f7a086164318a797
joaotomazio <jdtomazio@gmail.com>
2016-04-10 10:12:11 +0100

    complete constrctors on Dir

--- src/main/java/pt/tecnico/mydrive/service/ChangeDirectoryService.java
+++ src/main/java/pt/tecnico/mydrive/service/ChangeDirectoryService.java
@@ -31,7 +31,6 @@ public class ChangeDirectoryService extends MyDriveService{
         Login login = Login.getLoginByToken(token);
 
         String[] parts = path.split("/");
-
         Dir actual;
         int i;
 
@@ -50,6 +49,7 @@ public class ChangeDirectoryService extends MyDriveService{
         }
 
         if(actual.exists(parts[i++])){
+
         	if (actual.getFileByName(parts[i]).isDir()){
         		    Dir workingDir = actual.getDir(parts[i]);
 
