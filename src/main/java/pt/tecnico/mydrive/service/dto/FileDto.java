46557ae4ebf97199f680484db33b77514fa9a8f5
TonyFreire <tonifreire93@gmail.com>
2016-04-15 15:20:44 +0100

    added FileDto

---
+++ src/main/java/pt/tecnico/mydrive/service/dto/FileDto.java
@@ -0,0 +1,45 @@
+@package pt.tecnico.phonebook.service.dto;
+
+
+public class FileDto implements Comparable<FileDto> {
+    private int id;
+    private String name;
+    private String type;
+    private String perm;
+    private int size;
+    private String ownerUsername;
+    private DateTime lastModification;
+    
+    public FileDto (int id, String name, String type, String perm, int size, String ownerUsername, DateTime lastModification){
+        this.id=id;
+        this.name=name;
+        this.type=name;
+        this.perm=perm;
+        this.size=size;
+        this.ownerUsername=ownerUsername;
+        this.lastModification=lastModification;
+    }
+    public final int getId(){
+        return this.id;
+    }
+    public final String getName(){
+        return this.name;
+    }
+    public final String getType(){
+        return this.type;
+    } 
+    public final String getPerm(){
+        return this.perm;
+    } 
+    public final int getSize(){
+        return this.size;
+    } 
+    public final String getOwner(){
+        return this.ownerUsername;
+    }
+    @Override
+    public int compareTo(FileDto other) {
+         return getName().compareTo(other.getName());
+    }
+
+}
