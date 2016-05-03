00e1f02b1e4abbcb79c9a417b05dccaaabfd3370
Luis Pereira <luispereira1795@hotmail.com>
2016-05-03 22:31:01 +0100

    Changes in AddVariableTest

--- src/test/java/pt/tecnico/mydrive/service/AddVariableTest.java
+++ src/test/java/pt/tecnico/mydrive/service/AddVariableTest.java
@@ -12,12 +12,12 @@ public class AddVariableTest extends AbstractServiceTest{
     protected void populate(){
         
         MyDrive mydrive = MyDrive.getInstance();        
-        mydrive.createUser("antonio", "toni","toni","rwxd----");
+        mydrive.createUser("antonio", "toni","tonitoni12","rwxd----");
     }
 
     @Test
     public void successAddVariable(){
-        final long token = login("toni", "toni");
+        final long token = login("toni", "tonitoni");
         final String name = "aaa";
         final String value = "bbb";
         
@@ -32,7 +32,7 @@ public class AddVariableTest extends AbstractServiceTest{
 
     @Test
     public void sucessAddVariableOverrideAnotherName(){
-        final long token = login("toni", "toni");
+        final long token = login("toni", "tonitoni");
         final String name = "JJ";
         final String value = "Jorge";
         final String value1 = "Jesus";
@@ -45,17 +45,17 @@ public class AddVariableTest extends AbstractServiceTest{
         ArrayList<EnvDto> list = service1.result();
 
         EnvDto env = list.get(0);
-        assertEquals("Correct length", 1, list.length());
+        assertEquals("Correct length", 1, list.size());
         assertEquals("Correct name", "JJ", env.getName());
         assertEquals("Correct value", "Jesus", env.getValue());
     }
 
     @Test
     public void sucessAddMultipleVariables(){
-        final long token = login("toni", "toni");
+        final long token = login("toni", "tonitoni");
         final String name = "RV";
         final String value = "Rui";
-        final String name1 = "RV1"
+        final String name1 = "RV1";
         final String value1 = "Vitoria";
         
         AddVariableService service = new AddVariableService(token, name, value);
@@ -67,7 +67,7 @@ public class AddVariableTest extends AbstractServiceTest{
 
         EnvDto env = list.get(0);
         EnvDto env1 = list.get(1);
-        assertEquals("Correct length", 2, list.length());
+        assertEquals("Correct length", 2, list.size());
         assertEquals("Correct name", "RV", env.getName());
         assertEquals("Correct value", "Rui", env.getValue());
         assertEquals("Correct name", "RV1", env1.getName());
@@ -75,8 +75,8 @@ public class AddVariableTest extends AbstractServiceTest{
     }
 
     @Test(expected = InvalidEnvValuesException.class)
-    public void cantFindFile(){
-        final long token = login("toni", "toni");
+    public void AddVariableEmptyName(){
+        final long token = login("toni", "tonitoni");
         final String name = "";
         final String value = "Leicester";
         
@@ -85,8 +85,8 @@ public class AddVariableTest extends AbstractServiceTest{
     }
 
     @Test(expected = InvalidEnvValuesException.class)
-    public void cantFindFile(){
-        final long token = login("toni", "toni");
+    public void AddVariableNullName(){
+        final long token = login("toni", "tonitoni");
         final String name = null;
         final String value = "Vardy";
         
@@ -95,8 +95,8 @@ public class AddVariableTest extends AbstractServiceTest{
     }
 
     @Test(expected = InvalidEnvValuesException.class)
-    public void cantFindFile(){
-        final long token = login("toni", "toni");
+    public void AddVariableNullValue(){
+        final long token = login("toni", "tonitoni");
         final String name = "Jamie";
         final String value = null;
         
@@ -107,8 +107,10 @@ public class AddVariableTest extends AbstractServiceTest{
     @Test(expected = TokenDoesNotExistException.class)
     public void tokenExpired(){
         final long token = 1111111;
-        final String filename = "teste";
-        ReadFileService service = new ReadFileService(token, filename);
+        final String name = "Jamie";
+        final String value = "Vardy";
+
+        AddVariableService service = new AddVariableService(token, name, value);
         service.execute();
 
     }
