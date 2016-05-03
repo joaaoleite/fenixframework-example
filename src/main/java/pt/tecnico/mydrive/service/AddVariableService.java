d799fc0970fe6936e4ae5420b99e8fd72c3978a9
joaaoleite <joaogsleite@gmail.com>
2016-05-03 16:48:21 +0100

    AddVariableService

---
+++ src/main/java/pt/tecnico/mydrive/service/AddVariableService.java
@@ -0,0 +1,47 @@
+package pt.tecnico.mydrive.service;
+
+import java.util.ArrayList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import pt.ist.fenixframework.Atomic;
+import pt.tecnico.mydrive.domain.*;
+import pt.tecnico.mydrive.exception.*;
+
+import pt.tecnico.mydrive.service.dto.EnvDto;
+
+public class AddVariableService extends MyDriveService{
+	  MyDrive md = MyDrive.getInstance();
+
+    private String name;
+    private String value;
+    private ArrayList<EnvDto> res;
+    private long token;
+
+
+    public AddVariableService(long token){
+    	  super();
+    	  this.name = null;
+    	  this.value = null;
+    }
+    
+    public final ArrayList<EnvDto> result(){
+        return res;
+    }
+
+    protected final void dispatch() throws TokenDoesNotExistException, ExpiredTokenException, InvalidEnvValuesException {
+        
+        Login login = Login.getLoginByToken(token);
+
+        if(this.name==null || this.value==null || this.name.equals(""))
+            throw new InvalidEnvValuesException();
+        
+        login.setEnv(name,value);
+
+        res = new ArrayList<EnvDto>();
+        for(Env e : login.getEnvSet()){
+            res.add(new EnvDto(e.getName(),e.getValue()));
+        }
+    }
+}
