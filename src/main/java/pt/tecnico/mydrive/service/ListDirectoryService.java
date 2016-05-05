162e66d68edfce21a891b4c2581114ca497b223e
laurajbarroso <laurajbarroso@gmail.com>
2016-05-05 18:15:01 +0100

    ListDirectoryService with Dto

--- src/main/java/pt/tecnico/mydrive/service/ListDirectoryService.java
+++ src/main/java/pt/tecnico/mydrive/service/ListDirectoryService.java
@@ -1,28 +1,32 @@
 package pt.tecnico.mydrive.service;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import pt.tecnico.mydrive.service.dto.FileDto;
 
 import pt.ist.fenixframework.Atomic;
 import pt.tecnico.mydrive.domain.*;
 import pt.tecnico.mydrive.exception.*;
 
 public class ListDirectoryService extends MyDriveService{
-	MyDrive md = MyDrive.getInstance();
+    MyDrive md = MyDrive.getInstance();
 
-    private String res;
+    private List<FileDto> res;
     private long token;
 
 
     public ListDirectoryService(long token){
-    	super();
-
-    	this.token = token; 
-        
+        super();
+        this.token = token;      
     }
 
 
-    public String result(){
+    public List<FileDto> result(){
         return res; 
     }
 
@@ -36,7 +40,15 @@ public class ListDirectoryService extends MyDriveService{
         && !(login.getUser().getUsername().equals("root")))
         throw new InsufficientPermissionsException(login.getUser().getUsername());
         
+        res = new ArrayList<FileDto>();
+        
+        for(File f : workingDir.getFileSet())
+            res.add(new FileDto(f.getId(),f.getName(),f.getClass().getSimpleName(),f.getMask(),f.getSize(),f.getOwner().getUsername(),f.getLastModification()));
+        
+        res.add(new FileDto(workingDir.getId(),".",workingDir.getClass().getSimpleName(),workingDir.getMask(),workingDir.getSize(),workingDir.getOwner().getUsername(),workingDir.getLastModification()));
 
-        res = workingDir.listDir();
+        res.add(new FileDto(workingDir.getParent().getId(),"..",workingDir.getParent().getClass().getSimpleName(),workingDir.getParent().getMask(),workingDir.getParent().getSize(),workingDir.getParent().getOwner().getUsername(),workingDir.getParent().getLastModification()));
+
+        Collections.sort(res);
     }
 }
