180b9371877296735ca836e575e53f67c13df329
joaotomazio <jdtomazio@gmail.com>
2016-05-05 19:32:18 +0100

    ExecuteFileTest closes #70

---
+++ src/test/java/pt/tecnico/mydrive/service/ExecuteFileTest.java
@@ -0,0 +1,61 @@
+
+package pt.tecnico.mydrive.service;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import pt.tecnico.mydrive.domain.*;
+import pt.tecnico.mydrive.exception.*;
+
+public class ExecuteFileTest extends AbstractServiceTest{    
+    protected void populate(){
+        MyDrive mydrive = MyDrive.getInstance();
+        
+        mydrive.createUser("Halibio", "halib", "uhtuhtuht", "rwxd----");
+        User zetrigo = mydrive.createUser("Jose Trigo", "zetrigo", "tetetiti", "rwxd----");
+        mydrive.getRootDir().getDir("home").getDir("zetrigo").createPlainFile(mydrive.getUserByUsername("zetrigo"), "file.exe");
+        App app = mydrive.getRootDir().getDir("home").getDir("zetrigo").createApp(mydrive.getUserByUsername("zetrigo"), "app", "pt.tecnico.mydrive.domain.MyDrive.staticPrint");
+        zetrigo.createDefaultApp("exe", app);
+        
+   }
+    
+    @Test
+    public void success(){
+        final long token = login("zetrigo", "tetetiti");
+        final String path = "/home/zetrigo/file.exe";
+        String[] args = new String[2];
+        args[0] = "a";
+        args[1] = "b";
+        ExecuteFileService service = new ExecuteFileService(token, path, args);
+        service.execute();
+        String result = service.result(); 
+        
+        assertNotNull("No result from service", result);
+        assertEquals("Wrong result from service", "testCheck", result);
+    }
+
+
+    @Test(expected = FileDoesNotExistException.class)
+    public void FileDoesNotExist(){
+        final long token = login("zetrigo", "tetetiti");
+        final String path = "/home/zetrigo/start.exe";
+        String[] args = new String[2];
+        args[0] = "a";
+        args[1] = "b";
+        ExecuteFileService service = new ExecuteFileService(token, path, args);
+        service.execute();
+    }
+
+    @Test(expected = InsufficientPermissionsException.class)
+    public void FileDoesNotExist(){
+        final long token = login("halib", "uhtuhtuht");
+        final String path = "/home/zetrigo/file.exe";
+        String[] args = new String[2];
+        args[0] = "a";
+        args[1] = "b";
+        ExecuteFileService service = new ExecuteFileService(token, path, args);
+        service.execute();
+    }
+}
+
+
