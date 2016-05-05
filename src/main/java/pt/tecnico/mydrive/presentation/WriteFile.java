3bed8ccada42703d2bc0b6df2130708fe8e4174f
tiagofbfernandes <tiagofbfernandes@gmail.com>
2016-05-05 12:40:48 +0100

    Added writefile to presentation

---
+++ src/main/java/pt/tecnico/mydrive/presentation/WriteFile.java
@@ -0,0 +1,15 @@
+package pt.tecnico.mydrive.presentation;
+import pt.tecnico.mydrive.service.WriteFileService;
+
+public class WriteFile extends PbCommand {
+
+    public WriteFile(Shell sh) { super(sh, "update", "changes content of the path file"); }
+    public void execute(String[] args) {
+		if (args.length < 3)
+		    throw new RuntimeException("USAGE: "+name()+" <token> <filename> <text>");
+		else{
+			long token = login();
+		    new WriteFileService(token, args[0], args[1]).execute();
+		}
+    }
+}
