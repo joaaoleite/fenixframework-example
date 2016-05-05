b4f390a02c40428266faf0cbdafc82ec3061fa98
laurajbarroso <laurajbarroso@gmail.com>
2016-05-05 18:20:26 +0100

    test changes for new ListDirService

--- src/test/java/pt/tecnico/mydrive/service/ListDirectoryServiceTest.java
+++ src/test/java/pt/tecnico/mydrive/service/ListDirectoryServiceTest.java
@@ -3,8 +3,14 @@ package pt.tecnico.mydrive.service;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import pt.tecnico.mydrive.domain.*;
 import pt.tecnico.mydrive.exception.*;
+
+import pt.tecnico.mydrive.service.dto.FileDto;
 
 public class ListDirectoryServiceTest extends AbstractServiceTest{    
     protected void populate(){
@@ -25,50 +31,45 @@ public class ListDirectoryServiceTest extends AbstractServiceTest{
         final long token = login("halib", "uhtuhtuht");
         ListDirectoryService service = new ListDirectoryService(token);
         service.execute();
-        String[] list = service.result().split("\\r?\\n");
+        ArrayList<FileDto> list = (ArrayList)service.result();
        
         assertNotNull("Directory was not listed", list);
-       
-        String[] line1 = list[0].split(" ");
-        String[] line2 = list[1].split(" ");
-        String[] line3 = list[2].split(" ");
-        String[] line4 = list[3].split(" ");
-        String[] line5 = list[4].split(" ");
+        assertEquals("Directory was not listed", 5, list.size());
 
-        assertEquals("Wrong directory list 1", "Dir", line1[0]);
-        assertEquals("Wrong directory list 1", "rwxd----", line1[1]);
-        assertEquals("Wrong directory list 1", "5", line1[2]);
-        assertEquals("Wrong directory list 1", "halib", line1[3]);
-        assertEquals("Wrong directory list 1", "3", line1[4]);
-        assertEquals("Wrong directory list 1", ".", line1[6]);
+        assertEquals("Wrong directory list 1", "Dir", list.get(0).getType());
+        assertEquals("Wrong directory list 1", "rwxd----", list.get(0).getPerm());
+        assertEquals("Wrong directory list 1", 5, list.get(0).getSize());
+        assertEquals("Wrong directory list 1", "halib", list.get(0).getOwner());
+        assertEquals("Wrong directory list 1", 3, list.get(0).getId());
+        assertEquals("Wrong directory list 1", ".", list.get(0).getName());
 
-        assertEquals("Wrong directory list 2", "Dir", line2[0]);
-        assertEquals("Wrong directory list 2", "rwxdr-x-", line2[1]);
-        assertEquals("Wrong directory list 2", "5", line2[2]);
-        assertEquals("Wrong directory list 2", "root", line2[3]);
-        assertEquals("Wrong directory list 2", "1", line2[4]);
-        assertEquals("Wrong directory list 2", "..", line2[6]);
+        assertEquals("Wrong directory list 2", "Dir", list.get(1).getType());
+        assertEquals("Wrong directory list 2", "rwxdr-x-", list.get(1).getPerm());
+        assertEquals("Wrong directory list 2", 5, list.get(1).getSize());
+        assertEquals("Wrong directory list 2", "root", list.get(1).getOwner());
+        assertEquals("Wrong directory list 2", 1, list.get(1).getId());
+        assertEquals("Wrong directory list 2", "..", list.get(1).getName());
         
-        assertEquals("Wrong directory list 3", "PlainFile", line3[0]);
-        assertEquals("Wrong directory list 3", "rwxd----", line3[1]);
-        assertEquals("Wrong directory list 3", "0", line3[2]);
-        assertEquals("Wrong directory list 3", "halib", line3[3]);
-        assertEquals("Wrong directory list 3", "6", line3[4]);
-        assertEquals("Wrong directory list 3", "loans", line3[6]);
+        assertEquals("Wrong directory list 3", "PlainFile", list.get(2).getType());
+        assertEquals("Wrong directory list 3", "rwxd----", list.get(2).getPerm());
+        assertEquals("Wrong directory list 3", 0, list.get(2).getSize());
+        assertEquals("Wrong directory list 3", "halib", list.get(2).getOwner());
+        assertEquals("Wrong directory list 3", 6, list.get(2).getId());
+        assertEquals("Wrong directory list 3", "loans", list.get(2).getName());
 
-        assertEquals("Wrong directory list 4", "Dir", line4[0]);
-        assertEquals("Wrong directory list 4", "rwxd----", line4[1]);
-        assertEquals("Wrong directory list 4", "2", line4[2]);
-        assertEquals("Wrong directory list 4", "halib", line4[3]);
-        assertEquals("Wrong directory list 4", "7", line4[4]); 
-        assertEquals("Wrong directory list 4", "photos", line4[6]);
+        assertEquals("Wrong directory list 4", "Dir", list.get(3).getType());
+        assertEquals("Wrong directory list 4", "rwxd----", list.get(3).getPerm());
+        assertEquals("Wrong directory list 4", 2, list.get(3).getSize());
+        assertEquals("Wrong directory list 4", "halib", list.get(3).getOwner());
+        assertEquals("Wrong directory list 4", 7, list.get(3).getId()); 
+        assertEquals("Wrong directory list 4", "photos", list.get(3).getName());
 
-        assertEquals("Wrong directory list 5", "Dir", line5[0]);
-        assertEquals("Wrong directory list 5", "rwxd----", line5[1]);
-        assertEquals("Wrong directory list 5", "2", line5[2]);
-        assertEquals("Wrong directory list 5", "halib", line5[3]);
-        assertEquals("Wrong directory list 5", "5", line5[4]);
-        assertEquals("Wrong directory list 5", "test", line5[6]);
+        assertEquals("Wrong directory list 5", "Dir", list.get(4).getType());
+        assertEquals("Wrong directory list 5", "rwxd----", list.get(4).getPerm());
+        assertEquals("Wrong directory list 5", 2, list.get(4).getSize());
+        assertEquals("Wrong directory list 5", "halib", list.get(4).getOwner());
+        assertEquals("Wrong directory list 5", 5, list.get(4).getId());
+        assertEquals("Wrong directory list 5", "test", list.get(4).getName());
     }
     
     @Test
@@ -78,26 +79,24 @@ public class ListDirectoryServiceTest extends AbstractServiceTest{
         cd.execute();
         ListDirectoryService service = new ListDirectoryService(token);
         service.execute();
-        String[] list = service.result().split("\\r?\\n");
+        ArrayList<FileDto> list = (ArrayList) service.result();
        
         assertNotNull("Directory was not listed", list);
-       
-        String[] line1 = list[0].split(" ");
-        String[] line2 = list[1].split(" ");
+        assertEquals("Directory was not listed", 2, list.size());
 
-        assertEquals("Wrong directory list", "Dir", line1[0]);
-        assertEquals("Wrong directory list", "rwxdr-x-", line1[1]);
-        assertEquals("Wrong directory list", "2", line1[2]);
-        assertEquals("Wrong directory list", "root", line1[3]);
-        assertEquals("Wrong directory list", "2", line1[4]);
-        assertEquals("Wrong directory list", ".", line1[6]);
+        assertEquals("Wrong directory list", "Dir", list.get(0).getType());
+        assertEquals("Wrong directory list", "rwxdr-x-", list.get(0).getPerm());
+        assertEquals("Wrong directory list", 2, list.get(0).getSize());
+        assertEquals("Wrong directory list", "root", list.get(0).getOwner());
+        assertEquals("Wrong directory list", 2, list.get(0).getId());
+        assertEquals("Wrong directory list", ".", list.get(0).getName());
         
-        assertEquals("Wrong directory list", "Dir", line2[0]);
-        assertEquals("Wrong directory list", "rwxdr-x-", line2[1]);
-        assertEquals("Wrong directory list", "5", line2[2]);
-        assertEquals("Wrong directory list", "root", line2[3]);
-        assertEquals("Wrong directory list", "1", line2[4]);
-        assertEquals("Wrong directory list", "..", line2[6]);
+        assertEquals("Wrong directory list", "Dir", list.get(1).getType());
+        assertEquals("Wrong directory list", "rwxdr-x-", list.get(1).getPerm());
+        assertEquals("Wrong directory list", 5, list.get(1).getSize());
+        assertEquals("Wrong directory list", "root", list.get(1).getOwner());
+        assertEquals("Wrong directory list", 1, list.get(1).getId());
+        assertEquals("Wrong directory list", "..", list.get(1).getName());
     }
 
     @Test(expected = InsufficientPermissionsException.class)
