3cdaa6d9b64be9c84904f80f87d79e79287f80e8
tiagofbfernandes <tiagofbfernandes@gmail.com>
2016-05-06 17:18:02 +0100

    Added Execute to presentation

---
+++ src/main/java/pt/tecnico/mydrive/presentation/Execute.java
@@ -0,0 +1,19 @@
+package pt.tecnico.mydrive.presentation;
+import pt.tecnico.mydrive.service.ExecuteFileService;
+
+public class Execute extends PbCommand {
+
+    public Execute(Shell sh) { super(sh, "do", "executes the file in the path with the arguments"); }
+    public void execute(String[] args) {
+		if (args.length < 3)
+		    throw new RuntimeException("USAGE: "+name()+" <path> <args>");
+		else{
+			long token = login();
+			String[] str = new String[args.length-1];
+			for(int i = 1; i < args.length; i++){
+				str[i] =  args[i];
+			}
+		    new ExecuteFileService(token, args[0], str).execute();
+		}
+    }
+}
