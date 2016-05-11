df1e31ba6bbdc9c55c24282ef1ce07c5c63c9e12
tiagofbfernandes <tiagofbfernandes@gmail.com>
2016-05-11 15:09:02 +0100

    update with more text in  presentation

--- src/main/java/pt/tecnico/mydrive/presentation/WriteFile.java
+++ src/main/java/pt/tecnico/mydrive/presentation/WriteFile.java
@@ -5,11 +5,15 @@ public class WriteFile extends MyDriveCommand {
 
     public WriteFile(Shell sh) { super(sh, "update", "changes content of the path file"); }
     public void execute(String[] args) {
-		if (args.length != 2)
+		if (args.length < 2)
 		    throw new RuntimeException("USAGE: "+name()+" <path> <text>");
 		else{
 			long token = login();
-		    new WriteFileService(token, args[0], args[1]).execute();
+			String str = "";
+			for(int i = 1; i < args.length; i++){
+				str = str + " " + args[i];
+			}
+		    new WriteFileService(token, args[0], str).execute();
 		}
     }
 }
