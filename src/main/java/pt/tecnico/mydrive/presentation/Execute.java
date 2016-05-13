15832702fe0fa433f8b7ecec1d4944c6911806ba
joaotomazio <jdtomazio@gmail.com>
2016-05-13 17:34:04 +0100

    Presentation Execute closes #73

--- src/main/java/pt/tecnico/mydrive/presentation/Execute.java
+++ src/main/java/pt/tecnico/mydrive/presentation/Execute.java
@@ -3,17 +3,20 @@ import pt.tecnico.mydrive.service.ExecuteFileService;
 
 public class Execute extends MyDriveCommand {
 
-    public Execute(Shell sh) { super(sh, "do", "executes the file in the path with the arguments"); }
+    public Execute(Shell sh){
+        super(sh, "do", "executes the file in the path with the arguments");
+    }
+
     public void execute(String[] args) {
-		if (args.length < 1)
-		    throw new RuntimeException("USAGE: "+name()+" <path> <args>");
-		else{
-			long token = login();
-			String[] str = new String[args.length-1];
-			for(int i = 1; i < args.length; i++){
-				str[i] =  args[i];
-			}
-		    new ExecuteFileService(token, args[0], str).execute();
-		}
+        if (args.length < 1)
+            throw new RuntimeException("USAGE: "+name()+" <path> <args>");
+        else{
+          long token = login();
+          String[] str = new String[args.length-1];
+          for(int i = 1; i < args.length; i++){
+            str[i] =  args[i];
+          }
+            new ExecuteFileService(token, args[0], str).execute();
+        }
     }
 }
