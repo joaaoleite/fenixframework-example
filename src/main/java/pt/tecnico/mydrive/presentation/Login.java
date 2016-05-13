058047dd26526c45495a2e81a835ee0118f9c6a2
tiagofbfernandes <tiagofbfernandes@gmail.com>
2016-05-13 16:58:37 +0100

    bug fixes in login

--- src/main/java/pt/tecnico/mydrive/presentation/Login.java
+++ src/main/java/pt/tecnico/mydrive/presentation/Login.java
@@ -7,11 +7,18 @@ public class Login extends MyDriveCommand {
     public Login(Shell sh) { super(sh, "login", "login a user"); }
     public void execute(String[] args) {
 		
-        if (args.length != 2)
+		
+
+        if (args.length > 2 || args.length < 1)
 		        throw new RuntimeException("USAGE: "+name()+" <username> <password>");
 		
         else{
-            LoginService service = new LoginService(args[0],args[1]);
+        	String password="";
+        	if (args.length !=1){
+        		password=args[1];
+        	}
+        	
+            LoginService service = new LoginService(args[0],password);
 	          service.execute();
 	          login(args[0], service.result());
 	 	}
